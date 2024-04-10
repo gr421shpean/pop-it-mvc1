c5e7a03d396480aa2fd3eef09f1d4bcca52418f7
gr421shpean <149349454+gr421shpean@users.noreply.github.com>
2024-04-10 22:49:50 +0700

    Функция регистрации

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -5,6 +5,7 @@ namespace Controller;
 use Src\Request;
 use Model\Post;
 use Src\View;
+use Model\User;
 
 
 
@@ -21,4 +22,13 @@ class Site
     {
         return new View('site.hello', ['message' => 'hello working']);
     }
+
+    public function signup(Request $request): string
+    {
+        if ($request->method==='POST' && User::create($request->all())){
+            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+        }
+        return new View('site.signup');
+    }
+
 }
