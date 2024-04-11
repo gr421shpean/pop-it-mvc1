0cd6d0bfdad71e1e4e9883b4de06fdb9b734f3ce
gr421shpean <149349454+gr421shpean@users.noreply.github.com>
2024-04-11 18:15:22 +0700

    Проверка на роль admina

--- core/Src/Auth/Auth.php
+++ core/Src/Auth/Auth.php
@@ -1,14 +1,10 @@
 <?php
-
 namespace Src\Auth;
-
 use Src\Session;
-
 class Auth
 {
     //Свойство для хранения любого класса, реализующего интерфейс IdentityInterface
     private static IdentityInterface $user;
-
     //Инициализация класса пользователя
     public static function init(IdentityInterface $user): void
     {
@@ -17,14 +13,12 @@ class Auth
             self::login(self::user());
         }
     }
-
     //Вход пользователя по модели
     public static function login(IdentityInterface $user): void
     {
         self::$user = $user;
         Session::set('id', self::$user->getId());
     }
-
     //Аутентификация пользователя и вход по учетным данным
     public static function attempt(array $credentials): bool
     {
@@ -34,14 +28,12 @@ class Auth
         }
         return false;
     }
-
     //Возврат текущего аутентифицированного пользователя
     public static function user()
     {
         $id = Session::get('id') ?? 0;
         return self::$user->findIdentity($id);
     }
-
     //Проверка является ли текущий пользователь аутентифицированным
     public static function check(): bool
     {
@@ -50,12 +42,18 @@ class Auth
         }
         return false;
     }
-
     //Выход текущего пользователя
     public static function logout(): bool
     {
         Session::clear('id');
         return true;
     }
+    //Проверка, то что роль пользователя-админ
+    public static function checkRole(): bool
+    {
+        $userRole = User::where('id', '=', $_SESSION['id'])->first()->id_role;
 
+        if ($userRole === 1) return true;
+        else return false;
+    }
 }
